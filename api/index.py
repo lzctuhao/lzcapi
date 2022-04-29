106bef7d5c4ac45499806406f765a61ed8e5819c
lzctuhao <lzctuhao@outlook.com>
2022-04-29 22:56:32 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(str.encode())
         return
