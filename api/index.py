9d3cdacceeb6845f661efa489c7e56f7adc3dd78
lzctuhao <lzctuhao@outlook.com>
2022-04-29 22:25:02 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 
-str={'a':1}
+str="{'a':1}"
+
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(str.encode())
+        self.wfile.write(str.json())
         return
