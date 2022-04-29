b6d97a1821822927bb148736ae2292281903a51d
lzctuhao <lzctuhao@outlook.com>
2022-04-29 22:31:27 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,5 +8,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(str)
+        self.wfile.write(str.encode())
         return
