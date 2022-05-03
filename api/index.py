5acb3a19c46ae34ff65acd4383bed754af6bf6d2
lzctuhao <lzctuhao@outlook.com>
2022-05-03 17:15:14 +0800

    Update index.py
    测试wsgi

--- api/index.py
+++ api/index.py
@@ -1,18 +1,7 @@
-note="愉快的谈话胜似一切佳肴美馔。"
-by="lzc的匿名提问箱"
+from flask import Flask, Response
+app = Flask(__name__)
 
-import time
-from http.server import BaseHTTPRequestHandler
-
-time_str=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-str='{"note":"'+note+'" , "by":"'+by+'","time":"'+time_str+'"}'
-
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        self.wfile.write(str.encode())
-        return
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
