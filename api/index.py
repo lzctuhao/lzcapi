da69db9fee39abb303a35158918ac81586a8b542
lzctuhao <lzctuhao@outlook.com>
2022-05-03 18:13:58 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,6 @@ note="愉快的谈话胜似一切佳肴美馔。"
 by="lzc的匿名提问箱"
 
 import time
-
 from http.server import BaseHTTPRequestHandler
 
 time_str=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
@@ -15,5 +14,8 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write(dic)
+        
+        dic["path"]=self.path
+        
+        self.wfile.write(str(dic))
         return
