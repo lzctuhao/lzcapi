1080db94930e1bd5e9b762a70c3b371c39d6f5f5
lzctuhao <lzctuhao@outlook.com>
2025-02-28 23:24:06 +0800

    v1.3

--- api/hello.js
+++ api/hello.js
@@ -75,8 +75,8 @@ const handler = (req, res) => {
   if (req.method === 'GET') {
     res.status(200).json({ message: 'Hello from lzc\'s Vercel (GET)',time:getBeijingTime()});
   } else if (req.method === 'POST') {
-    const { name } = req.body;
-    res.status(200).json({ message: `Hello, ${name}! (POST)`,time:getBeijingTime(),req:req});
+    const name=req.body.name || "Anonymous";
+    res.status(200).json({ message: `Hello, ${name}! (POST)`,time:getBeijingTime()});
   } else {
     res.status(405).json({ error: 'Method Not Allowed',time:getBeijingTime()});
   }
